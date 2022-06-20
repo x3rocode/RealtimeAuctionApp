c500cadf5f1d70cf8dfe55f81c08759ff7ac1824
fascinate98 <51151970+fascinate98@users.noreply.github.com>
2022-06-21 00:34:31 +0900

    login2

--- app/src/main/java/com/esteel4u/realtimeauctionapp/view/ui/LoginActivity.kt
+++ app/src/main/java/com/esteel4u/realtimeauctionapp/view/ui/LoginActivity.kt
@@ -1,7 +1,20 @@
 package com.esteel4u.realtimeauctionapp.view.ui
 
+import android.content.Intent
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.esteel4u.realtimeauctionapp.R
+import com.esteel4u.realtimeauctionapp.databinding.ActivityLoginBinding
+import com.esteel4u.realtimeauctionapp.databinding.ActivityWelcomeBinding
 
-class LoginActivity : AppCompatActivity()
-{
+class LoginActivity : AppCompatActivity() {
+    private var activityLoginBinding: ActivityLoginBinding ?= null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+
+
+    }
 }
